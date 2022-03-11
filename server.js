3b5c88a4aa1824f45fd8355c51a13df0bbf49fb7
carlosgitub <carlos.dh08@gmail.com>
2022-03-11 17:56:30 -0500

    servidor de pruebas

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(favicon(path.join(__dirname, 'dist/favicon.png')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'));
+
+console.log('Listening on port: ' + app.get('port'));
+
+module.exports = app;
